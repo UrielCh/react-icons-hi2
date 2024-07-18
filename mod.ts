9ab4a290d829ce4392962ffc454adba48b29d91b
urielch <uriel.chemouni@gmail.com>
2024-07-18 09:55:06 +0200

    update doc

--- mod.ts
+++ mod.ts
@@ -10,39 +10,56 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/hi2)
  * 
- * ## import_map.json
+ * ## install packahe
  * 
- * For a transparent usage:
- * 
- * ```json
- * {
- *   "imports": {
- *     "@preact-icons/common": "jsr:@preact-icons/common@^1.0.10",
- *     "preact": "npm:preact@10.22.1",
- *     "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *     "preact/hooks": "npm:preact@10.22.1/hooks",
- *     "react-icons/hi2": "jsr:@preact-icons/hi2@^1.0.11/mod.ts",
- *     "react-icons/hi2/": "jsr:@preact-icons/hi2@^1.0.11/ico/",
- *   }
- * }
+ * ```bash
+ * deno add @preact-icons/hi2
+ * dnpx jsr add @preact-icons/hi2
+ * pnpm dlx jsr add @preact-icons/hi2
+ * bunx jsr add @preact-icons/hi2
  * ```
  * 
- * ## Import an icon without import_map by and afer loading all icons from the lib hi2
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
  * 
- * ```ts
- * import { HiAcademicCap } from "jsr:preact-icons/hi2@1.0.11/mod.ts"
+ * 
+ * # Heroicons 2 icons for preact
+ * 
+ * [![JSR](https://jsr.io/badges/@preact-icons/hi2)](https://jsr.io/@preact-icons/hi2)
+ * 
+ * **License** [MIT](https://opensource.org/licenses/MIT)
+ * 
+ * **Project** [https://github.com/tailwindlabs/heroicons](https://github.com/tailwindlabs/heroicons)
+ * 
+ * [See available icons here](https://react-icons.deno.dev/hi2)
+ * 
+ * ## install packahe
+ * 
+ * ```bash
+ * deno add @preact-icons/hi2
+ * dnpx jsr add @preact-icons/hi2
+ * pnpm dlx jsr add @preact-icons/hi2
+ * bunx jsr add @preact-icons/hi2
  * ```
  * 
- * ## import_map import an icon from all icons
+ * You may need to update your preact mapping to:
+ *  - "preact": "npm:preact@10.22.1",
+ *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  - "preact/hooks": "npm:preact@10.22.1/hooks",
+ * 
+ * 
+ * ## import an icon from all icons
  * 
  * ```ts
- * import { HiAcademicCap } from "react-icons/hi2"
+ * import { HiAcademicCap } from "@preact-icons/hi2"
  * ```
  * 
  * ## import a single icon, downloading just one icon
  * 
  * ```ts
- * import { HiAcademicCap } from "react-icons/hi2/HiAcademicCap.ts"
+ * import { HiAcademicCap } from "react-icons/hi2/HiAcademicCap"
  * ```
  * 
  * or using default export
